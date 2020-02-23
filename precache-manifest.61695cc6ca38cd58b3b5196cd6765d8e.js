19613f23f984b850c4dd90131010c1bcaf67c1fd
Ryan Berckmans <ryanberckmans@gmail.com>
2020-02-23 14:55:18 -0600

    Updates

---
+++ precache-manifest.61695cc6ca38cd58b3b5196cd6765d8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9972666a552b70e03851db546f7250b3",
+    "url": "/ethereum-monetary-policy/index.html"
+  },
+  {
+    "revision": "22dc9fa9234bd1aa3c5f",
+    "url": "/ethereum-monetary-policy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a7386838e94123524d58",
+    "url": "/ethereum-monetary-policy/static/js/2.6ffd727a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ethereum-monetary-policy/static/js/2.6ffd727a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dc9fa9234bd1aa3c5f",
+    "url": "/ethereum-monetary-policy/static/js/main.d1cb2d20.chunk.js"
+  },
+  {
+    "revision": "b1deb6016fb55aecb3f1",
+    "url": "/ethereum-monetary-policy/static/js/runtime-main.8ff860ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ethereum-monetary-policy/static/media/logo.5d5d9eef.svg"
+  }
+]);
